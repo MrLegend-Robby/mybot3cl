691590e058c14372b788255209b0a837a9ca20b1
MrLegend-Robby <35876269+MrLegend-Robby@users.noreply.github.com>
2018-06-02 17:15:25 +0300

    mybot3cl

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const bott = new Discord.Client();
 
 
-const colors = ['92ce14', '4bd588', '4bd5b9', 'd54b76', '00edff', '0077ff', 'f2ff00', 'cbd073', 'd09573', 'f2070b',];
+const colors = ['92ce14', '4bd588'];
 function color () {
   colors.forEach(function (item, number) {
       setTimeout(function () {bott.guilds.get('443386238838374400').roles.get('451838923325308929').setColor(item).catch();if(number === colors.length-1) setTimeout(function () {color()}, 1000)}, number*1500);
